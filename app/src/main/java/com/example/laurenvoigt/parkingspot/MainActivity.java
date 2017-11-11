44c2234cfdd1c04a660e4b4d0f84263345a5c035
laurenvoigt <lurn11@yahoo.com>
2017-11-10 18:52:14 -0600

    Implemented Google Maps API, button clicks, ParkingSpot logo

--- app/src/main/java/com/example/laurenvoigt/parkingspot/MainActivity.java
+++ app/src/main/java/com/example/laurenvoigt/parkingspot/MainActivity.java
@@ -20,11 +20,38 @@ import android.widget.Button;
 import android.content.Context;
 import android.widget.RelativeLayout;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 import static com.example.laurenvoigt.parkingspot.R.id.action_context_bar;
 import static com.example.laurenvoigt.parkingspot.R.id.nv;
 
 public class MainActivity extends AppCompatActivity {
+    TextView userName;
+    TextView userId;
+    LoginButton login_button;
+    CallbackManager callbackManager;
+    ProfilePictureView profilePictureView;
     private DrawerLayout dl;
     private DrawerLayout nv;
     private ActionBarDrawerToggle mToggle;
@@ -49,10 +76,13 @@ public class MainActivity extends AppCompatActivity {
 
         //Added Stuff Here
         //https://stackoverflow.com/questions/5658675/replacing-a-fragment-with-another-fragment-inside-activity-group
+        //make fragment default page when open application
+
 
         FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
         tx.replace(R.id.flcontent, new HomePage());
         tx.commit();
+
 
         dl = (DrawerLayout) findViewById(R.id.dl);
         mToggle = new ActionBarDrawerToggle(this, dl, R.string.open, R.string.close);
@@ -78,6 +108,12 @@ public class MainActivity extends AppCompatActivity {
         //homepage as id?
         getSupportFragmentManager().beginTransaction().replace(R.id.flcontent, new HomePage()).addToBackStack(null).commit();
 
+        //New stuff here
+        //super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplication());
+        //setContentView(R.layout.activity_main);
+        initializeControls();
+        loginWithFB();
 
     }
 
@@ -127,5 +163,66 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //NEW STUFF HERE
+    private void initializeControls(){
+        callbackManager = CallbackManager.Factory.create();
+        userName = (TextView)findViewById(R.id.userName);
+        userId = (TextView)findViewById(R.id.userId);
+        profilePictureView = (ProfilePictureView)findViewById(R.id.picture);
+        login_button = (LoginButton)findViewById(R.id.login_button);
+    }
+
+    private void loginWithFB(){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("Main", response.toString());
+                                setProfileToView(object);
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+            @Override
+            public void onCancel() {
+                userName.setText("Login Cancelled.");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                userName.setText("Login Error: " + error.getMessage());
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void setProfileToView(JSONObject jsonObject) {
+        try {
+
+            userName.setText("Name:" + jsonObject.getString("name"));
+            userId.setText("UserId: " + jsonObject.getString("id"));
+            profilePictureView.setPresetSize(ProfilePictureView.NORMAL);
+            profilePictureView.setProfileId(jsonObject.getString("id"));
+
+            //infoLayout.setVisibility(View.VISIBLE);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
 
